351aef1a39a90a0702fe33911e32310f9590329d
Manecoon <stas_on.85@mail.ru>
2023-07-07 20:46:00 +0300

    6. Правила Checkstyle [#504981  [#504981 #428571]]

---
+++ src/main/java/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package checkstyle;
+
+public class Broken {
+    String name;
+    public String surname;
+    public static final String NEWVALUE = "";
+    private int sizeOfEmpty = 10;
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void echo() {
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    Broken() {
+    }
+}
