7c51e5cd45152630644f1583668054ea47cdcdf5
Manecoon <stas_on.85@mail.ru>
2023-07-08 14:36:39 +0300

    6. Правила Checkstyle [#504981  [#504981 #428571]]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,10 +1,13 @@
-package ru.job4j.tracker.checkstyle;
+package ru.job4j.checkstyle;
 
 public class Broken {
     String name;
     public String surname;
     public static final String NEWVALUE = "";
     private int sizeOfEmpty = 10;
+
+    Broken() {
+    }
 
     void media(Object obj) {
         if (obj != null) {
@@ -16,8 +19,5 @@ public class Broken {
     }
 
     void method(int a, int b, int c, int d, int e, int f, int g) {
-    }
-
-    Broken() {
     }
 }
