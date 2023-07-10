189a999a32175eb431144f165732ce5ae2a4ff41
Manecoon <stas_on.85@mail.ru>
2023-07-10 19:30:11 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587 # [#122587 #428935]]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -3,6 +3,7 @@ package ru.job4j.oop;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 public class PointTest {
 
@@ -10,29 +11,26 @@ public class PointTest {
     public void when00to20then2() {
         Point a = new Point(0, 0);
         Point b = new Point(2, 0);
-        a.distance(b);
-        double dist = Math.round(a.distance(b));
+        double dist = a.distance(b);
         double expected = 2;
-        assertThat(dist).isEqualTo(expected);
+        assertThat(dist).isCloseTo(expected, offset(0.5));
     }
 
     @Test
     public void when03to20then3dot5() {
         Point a = new Point(0, 3);
         Point b = new Point(2, 0);
-        a.distance(b);
-        double dist = Math.round(a.distance(b));
-        double expected = 4;
-        assertThat(dist).isEqualTo(expected);
+        double dist = a.distance(b);
+        double expected = 3.5;
+        assertThat(dist).isCloseTo(expected, offset(0.5));
     }
 
     @Test
     public void when41to20then2() {
         Point a = new Point(4, 1);
         Point b = new Point(2, 0);
-        a.distance(b);
-        double dist = Math.round(a.distance(b));
+        double dist = a.distance(b);
         double expected = 2;
-        assertThat(dist).isEqualTo(expected);
+        assertThat(dist).isCloseTo(expected, offset(0.5));
     }
 }
