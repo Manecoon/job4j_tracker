b6cd5b2f4d28ccd929e31859ccbe9caa4018c385
Manecoon <stas_on.85@mail.ru>
2023-07-18 14:47:31 +0300

    8. Date. Отображение даты. [#310017  [#310017]]

--- src/main/java/ru/job4j/tracker/ru/job4j/StartUi.java
+++ src/main/java/ru/job4j/tracker/ru/job4j/StartUi.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUi {
     public static void main(String[] args) {
-        Item item = new Item ();
+        Item item = new Item();
         System.out.println("Текущие дата и время после форматирования: " + item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
     }
 }
