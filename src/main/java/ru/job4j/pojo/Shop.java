d7f225ccb40f12a0b13330863805123d910b24be
Manecoon <stas_on.85@mail.ru>
2023-07-19 13:40:47 +0300

    3.1. Массив с пустыми ячейками [#176470  [#176470]]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -10,8 +10,9 @@ public class Shop {
 
     public static int indexOfNull(Product[] products) {
         for (int i = 0; i < products.length; i++) {
-            if (products[i] == null)
+            if (products[i] == null) {
                 return i;
+            }
         }
         return -1;
     }
