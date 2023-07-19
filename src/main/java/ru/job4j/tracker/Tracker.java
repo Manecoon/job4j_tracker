0679c07de5685cd11b1577914c4573fa9b433403
Manecoon <stas_on.85@mail.ru>
2023-07-19 18:41:58 +0300

    5. Tracker - хранилище [#396  [#396]]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,30 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                result[index] = items[i];
+                index++;
+            }
+        }
+        return Arrays.copyOf(result, index);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] find = new Item[size];
+        int ind = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                find[ind] = items[i];
+                ind++;
+            }
+        }
+        return Arrays.copyOf(find, ind);
     }
 
     public Item findById(int id) {
