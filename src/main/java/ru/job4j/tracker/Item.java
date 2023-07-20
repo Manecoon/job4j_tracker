20f5b4da54ae2804b16f86433e62d9a66618ec5e
Manecoon <stas_on.85@mail.ru>
2023-07-20 20:54:14 +0300

    10. toString [#310018  [#310018]]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,14 @@
 package ru.job4j.tracker;
 
+import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER
+            = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
     }
@@ -37,5 +40,13 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER)
+                + '}';
     }
 }
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,7 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+    Item proba = new Item();
+        System.out.println(proba); }
+}
